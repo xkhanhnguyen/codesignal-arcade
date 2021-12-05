9060bf5a4f427cc54dc65b95a2f97d9a35a3cb4d
XuanKhanh Nguyen <ngxuankhanhdl@gmail.com>
2021-12-05 08:40:20 -0500

    new file

---
+++ Intro/Diving Deeper/35_firstDegit.py
@@ -0,0 +1,19 @@
+"""
+Find the leftmost digit that occurs in a given string.
+
+Example
+
+For inputString = "var_1__Int", the output should be
+solution(inputString) = '1';
+For inputString = "q2q-q", the output should be
+solution(inputString) = '2';
+For inputString = "0ss", the output should be
+solution(inputString) = '0'.
+"""
+
+def solution(inputString):
+    if inputString.isdegit():
+        return inputString
+
+
+print(solution("var_1__Int"))
