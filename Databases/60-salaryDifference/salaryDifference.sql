bcf7ffabd575d2eaf8eaead1fff6fc5542e8ca7b
XuanKhanh Nguyen <ngxuankhanhdl@gmail.com>
2021-12-28 18:22:59 -0500

    #60

--- Databases/60-salaryDifference/salaryDifference.sql
+++ Databases/60-salaryDifference/salaryDifference.sql
@@ -1,9 +1,12 @@
-CREATE PROCEDURE salaryDifference()
+CREATE PROCEDURE solution()
 BEGIN
-    SET @maxSal = (SELECT max(salary) FROM employees);
-    SET @minSal = (SELECT min(salary) FROM employees);
-	SET @maxSum = (SELECT @maxSal * COUNT(*) FROM employees WHERE salary =@maxSal);
-    SET @minSum = (SELECT @minSal * COUNT(*) FROM employees WHERE salary =@minSal);
+	/* Write your SQL here. Terminate each statement with a semicolon. */
+    SET @max_salary = (SELECT MAX(salary) FROM employees);
+    SET @min_salary = (SELECT MIN(salary) FROM employees);
+    SET @total_max = (SELECT @max_salary * 
+                    COUNT(*) FROM employees WHERE salary = @max_salary);
+    SET @total_min = (SELECT @min_salary * 
+                    COUNT(*) FROM employees WHERE salary = @min_salary);
+    SELECT IFNULL(@total_max - @total_min, 0) as salary_diff;
     
-    SELECT IFNULL(@maxSum - @minSum, 0) as difference;
 END
