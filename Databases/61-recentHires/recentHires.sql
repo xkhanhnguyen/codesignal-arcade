e508155096c999f953f826c35d72c2951b8fba17
XuanKhanh Nguyen <ngxuankhanhdl@gmail.com>
2022-01-03 10:02:30 -0500

    #44

--- Databases/61-recentHires/recentHires.sql
+++ Databases/61-recentHires/recentHires.sql
@@ -1,14 +1,14 @@
-CREATE PROCEDURE recentHires()
+CREATE PROCEDURE solution()
 BEGIN
-SELECT name as names FROM (
-    
-(SELECT name, 1 as filter FROM (SELECT name from pr_department ORDER BY date_joined DESC LIMIT 5) pr ORDER BY name)
-UNION ALL
-(SELECT  name, 2 as filter FROM (SELECT name from it_department ORDER BY date_joined DESC LIMIT 5) it ORDER BY name)
-UNION ALL
-(SELECT name, 3 as filter FROM (SELECT name from sales_department ORDER BY date_joined DESC LIMIT 5) sales ORDER BY name)
-
-    ) a ORDER BY filter, name;
-
-	
+	/* Write your SQL here. Terminate each statement with a semicolon. */
+    SELECT name as names FROM
+    (
+        (SELECT 1 department, name FROM pr_department ORDER BY date_joined DESC LIMIT 5) 
+        UNION ALL
+        (SELECT 2, name FROM it_department ORDER BY date_joined DESC LIMIT 5)
+        UNION ALL
+        (SELECT 3, name FROM sales_department ORDER BY date_joined DESC LIMIT 5)
+    ) names 
+    ORDER BY department, name;
 END
+
