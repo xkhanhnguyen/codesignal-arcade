3ce78d1acfa8f1f1155cfe7e6594a20657e38c97
XuanKhanh Nguyen <ngxuankhanhdl@gmail.com>
2022-01-10 10:11:54 -0500

    #52

---
+++ Intro/Land of Logic/52_longestWord.py
@@ -0,0 +1,21 @@
+"""
+Define a word as a sequence of consecutive English letters. Find the longest word from the given string.
+
+Example
+
+For text = "Ready, steady, go!", the output should be
+solution(text) = "steady".
+"""
+import re
+def solution(text):
+    text = re.split('[^a-zA-Z0-9]+',text). # split on non-alphanumeric characters
+    print(text)
+    dicts = {}
+    for word in text:
+        dicts[word] = len(word)
+    
+    return max(dicts, key=dicts.get)
+
+
+print(solution("Ready, steady, go!"))
+print(solution("ab-CDE-fg_hi"))
