4bcc1035f69a34c3e3affb628c4b013a3da042dd
XuanKhanh Nguyen <ngxuankhanhdl@gmail.com>
2022-01-10 20:30:00 -0500

    #58

--- Intro/Land of Logic/57_fileNaming.py
+++ Intro/Land of Logic/57_fileNaming.py
@@ -1,5 +1,7 @@
 """
-You are given an array of desired filenames in the order of their creation. Since two files cannot have equal names, the one which comes later will have an addition to its name in a form of (k), where k is the smallest positive integer such that the obtained name is not used yet.
+You are given an array of desired filenames in the order of their creation. 
+Since two files cannot have equal names, the one which comes later will have an addition to its name in a form of (k), 
+where k is the smallest positive integer such that the obtained name is not used yet.
 
 Return an array of names that will be given to the files.
 
@@ -8,3 +10,16 @@ Example
 For names = ["doc", "doc", "image", "doc(1)", "doc"], the output should be
 solution(names) = ["doc", "doc(1)", "image", "doc(1)(1)", "doc(2)"].
 """
+
+def solution(names):
+    
+    for i in range(len(names)):
+        if names[i] in names[:i]:
+            count = 1
+            while names[i] + "("+str(count)+")" in names[:i]:
+                count += 1
+            names[i] +=  "("+str(count)+")"
+    return names
+
+print(solution(["doc", "doc", "image", "doc(1)", "doc"]))
+
