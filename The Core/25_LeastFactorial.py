450ecbb67b1355a33492d80dbfceddab1f996650
XuanKhanh Nguyen <ngxuankhanhdl@gmail.com>
2022-01-15 08:33:20 -0500

    solve #25

---
+++ The Core/25_LeastFactorial.py
@@ -0,0 +1,26 @@
+"""
+Given an integer n, find the minimal k such that
+
+k = m! (where m! = 1 * 2 * ... * m) for some integer m;
+k >= n.
+In other words, find the smallest factorial which is not less than n.
+
+Example
+
+For n = 17, the output should be
+solution(n) = 24.
+
+17 < 24 = 4! = 1 * 2 * 3 * 4, while 3! = 1 * 2 * 3 = 6 < 17).
+"""
+
+
+
+def solution(n):
+    numb = 1
+    ans = 1
+    while ans < n:
+        ans *= numb
+        numb += 1
+
+    return ans
+print(solution(24))
