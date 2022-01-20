5378f7fb48b27d9965986b83284ac1d2b4f3fcbf
XuanKhanh Nguyen <ngxuankhanhdl@gmail.com>
2022-01-20 16:47:45 -0500

    finish #51

---
+++ The Core/51_EncloseInBrackets.py
@@ -0,0 +1,11 @@
+"""
+Given a string, enclose it in round brackets.
+
+Example
+
+For inputString = "abacaba", the output should be
+solution(inputString) = "(abacaba)".
+
+"""
+def solution(inputString):
+    return ("(" + inputString + ")")
