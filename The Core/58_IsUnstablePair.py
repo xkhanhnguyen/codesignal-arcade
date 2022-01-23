95d838307d4374f2445164b822bda524fec418ae
XuanKhanh Nguyen <ngxuankhanhdl@gmail.com>
2022-01-23 08:45:27 -0500

    #58

---
+++ The Core/58_IsUnstablePair.py
@@ -0,0 +1,29 @@
+"""
+Some file managers sort filenames taking into account case of the letters, 
+others compare strings as if all of the letters are of the same case. 
+That may lead to different ways of filename ordering.
+
+Call two filenames an unstable pair if their ordering depends on the case.
+
+To compare two filenames a and b, find the first position i at which a[i] # b[i]. 
+If a[i] < b[i], then a < b. Otherwise a > b. 
+If such position doesn't exist, the shorter string goes first.
+
+Given two filenames, check whether they form an unstable pair.
+
+Example
+
+For filename1 = "aa" and filename2 = "AAB", the output should be
+solution(filename1, filename2) = true.
+
+Because "AAB" < "aa", but "AAB" > "AA".
+
+For filename1 = "A" and filename2 = "z", the output should be
+solution(filename1, filename2) = false.
+
+Both "A" < "z" and "a" < "z".
+"""
+
+def solution(filename1, filename2):
+    return (filename1.lower() < filename2.lower()) != (filename1 < filename2)
+print(solution("aa", "AAB"))
